5ddc098a0ace1d31383a7d35f4f49f5615c5b3cc
JonathanE2607 <159081123+JonathanE2607@users.noreply.github.com>
2024-09-25 08:56:34 -0500

    connect routes in react

--- senasoft-front/tailwind.config.js
+++ senasoft-front/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fondo':'#55E116',
+        'links': '#FA6132',
+        'fondo2': '#F8F9FF',
+      }
+    },
   },
   plugins: [],
 }
